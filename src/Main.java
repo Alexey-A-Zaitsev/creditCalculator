8299078cab1c781c1fc44d27158c2a235464b5ca
Alex Zaitsev <saizewalex@gmail.com>
2022-11-18 16:04:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        double s = 1_000_000; // сумма займа
+
+        creditPaymentService service = new creditPaymentService();
+
+        System.out.println();
+        System.out.println("Ежемесячный платеж по кредиту на срок 1 год составит:");
+        System.out.printf("%.0f", service.calculate(12) * s);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Ежемесячный платеж по кредиту на срок 2 года составит:");
+        System.out.printf("%.0f", service.calculate(24) * s);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Ежемесячный платеж по кредиту на срок 3 года составит:");
+        System.out.printf("%.0f", service.calculate(36) * s);
+
+
+
+
+    }
+}
---
+++ src/creditPaymentService.java
@@ -0,0 +1,11 @@
+public class creditPaymentService {
+
+    double i = 9.99 / 12 / 100; // Месячная процентная ставка
+
+    public double calculate(double n) {
+        double result;
+        result = i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1); // Расчет коэффициента аннуитета
+        return result;
+    }
+
+}
